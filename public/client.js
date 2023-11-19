001a1fde20489c8047d73f80efe37409d0daf7b7
Will Dunlop <54122840+wjdunlop@users.noreply.github.com>
2023-11-19 00:12:32 +0000

    update

--- public/client.js
+++ public/client.js
@@ -1,49 +1,200 @@
-$(function() {
-  console.log('hello world :o');
+var temporaryData = {
+  intake: [],
+  need: [],
+  want: []
+};
 
-  function submitForm(endpoint, listId) {
-    $('form.' + endpoint).submit(function(event) {
-      event.preventDefault();
-      var resource = $('form.' + endpoint + ' input[placeholder="resource"]').val();
-      var amount = $('form.' + endpoint + ' input[placeholder="amount"]').val();
-      var byWhomForWhom = $('form.' + endpoint + ' input[placeholder="' + (endpoint === 'intakes' ? 'by whom' : 'for whom') + '"]').val();
+// Define the submitForm function outside the ready block
+function submitForm(type, endpoint) {
+  // Use event delegation to handle form submissions
+  $('body').on('submit', 'form.' + endpoint + '-form', function(event) {
+    console.log('submitting form.' + endpoint + '-form');
+    event.preventDefault();
 
-      var data = {
-        resource: resource,
-        amount: amount,
-        byWhomForWhom: byWhomForWhom
-      };
+    var form = $(this);
+    var formData = {
+      resource: form.find('input#' + endpoint + '-resource').val(),
+      amount: form.find('input#' + endpoint + '-amount').val(),
+      byWhomForWhom: form.find('input#' + endpoint + '-whom').val()
+    };
 
-      $.post('/' + endpoint + '?' + $.param(data), function() {
-        $('<li></li>').text(resource + ' - ' + amount + ' - ' + byWhomForWhom).appendTo('ul#' + listId);
-        $('form.' + endpoint + ' input').val('');
-        $('form.' + endpoint + ' input').focus();
-      });
+    addRow(type, formData);
+
+    form.trigger('reset');
+    form.find('input:first').focus();
+  });
+}
+
+function commitTxn(type, endpoint) {
+  $('#'+ type +'-commit-button').on('click', function() {
+    console.log('Committing ' + temporaryData[type].length + type + ' transactions...');
+
+    // Assuming you have a function to format data for the endpoint
+    var formattedData = formatDataForEndpoint(type);
+    console.log(formattedData)
+    
+    // Assuming you have an endpoint URL for committing transactions
+    var commitEndpoint = '/commit/' + endpoint;
+
+    // Send a POST request to the commit endpoint
+    $.ajax({
+      url: commitEndpoint,
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(formattedData),
+      success: function(response) {
+        console.log(type + ' transactions committed successfully:', response);
+        
+        // After successful commit, clear the temporary data and update the display
+        temporaryData[type] = [];
+        displayTemporaryData(type);
+      },
+      error: function(error) {
+        console.error('Error committing ' + type + ' transactions:', error);
+      }
     });
+  });
+}
+
+// Example function to format data for the endpoint
+function formatDataForEndpoint(type) {
+  // Assuming the data structure matches the endpoint expectations
+  return temporaryData[type];
+}
+
+// Define the addRow function
+function addRow(type, formData) {
+
+  var list = $('#' + type);
+  var listItem = $('<li>' + formData.resource + ' (' + formData.amount + ') - ' + formData.byWhomForWhom + '<button class="delete-button">Delete</button></li>');
+
+  // Add delete button click event
+  listItem.find('.delete-button').click(function() {
+    listItem.remove();
+    updateTemporaryData(type, formData, 'delete');
+  });
+
+  list.append(listItem);
+  updateTemporaryData(type, formData, 'add');
+}
+
+// Define the updateTemporaryData function
+function updateTemporaryData(type, formData, action) {
+  if (action === 'add') {
+    console.log('adding to ' + type)
+    temporaryData[type].push(formData);
+    console.log('Temporary data');
+    console.groupCollapsed(type);
+    console.log(temporaryData[type].length);
+  } else if (action === 'delete') {
+    var index = temporaryData[type].findIndex(function(entry) {
+      return (
+        entry.resource === formData.resource &&
+        entry.amount === formData.amount &&
+        entry.byWhomForWhom === formData.byWhomForWhom
+      );
+    });
+
+    if (index !== -1) {
+      temporaryData[type].splice(index, 1);
+    }
   }
 
-  submitForm('intake', 'intakes');
-  submitForm('need', 'needs');
-  submitForm('want', 'wants');
+  updateCommitButtonVisibility();
+  displayTemporaryData(type);
+}
 
-  // Fetch existing intakes
-  $.get('/intake', function(intakes) {
-    intakes.forEach(function(intake) {
-      $('<li></li>').text(intake.resource + ' - ' + intake.amount + ' - ' + intake.byWhomForWhom).appendTo('ul#intakes');
-    });
+// Define the displayTemporaryData function
+function displayTemporaryData(type) {
+  console.log('displaying ' + type + ' temporary data...');
+
+  var temporarySection = $('.txn-window #temporary-' + type + '-table tbody');
+  console.log('#temporary-' + type + '-table');
+  var dataList = temporaryData[type];
+
+  temporarySection.empty(); // Clear the temporary section
+
+  if (dataList.length > 0) {
+      dataList.forEach(function (entry) {
+          var row = $('<tr>');
+          row.append('<td>' + entry.resource + '</td>');
+          row.append('<td>' + entry.amount + '</td>');
+          row.append('<td>' + entry.byWhomForWhom + '</td>');
+          row.append('<td><button class="delete-button">Delete</button></td>');
+
+          // Add delete button click event
+          row.find('.delete-button').click(function () {
+              row.remove();
+              updateTemporaryData(type, entry, 'delete');
+          });
+
+          temporarySection.append(row);
+      });
+      console.log('appending rows...');
+  } else {
+      temporarySection.append('<tr><td colspan="4">No uncommitted ' + type + ' txns.</td></tr>');
+  }
+}
+
+// Define the updateCommitButtonVisibility function
+function updateCommitButtonVisibility() {
+  var commitButton = $('#commit-button');
+  var hasModifications = Object.values(temporaryData).some(function(data) {
+    return data.length > 0;
   });
 
-  // Fetch existing needs
-  $.get('/need', function(needs) {
-    needs.forEach(function(need) {
-      $('<li></li>').text(need.resource + ' - ' + need.amount + ' - ' + need.byWhomForWhom).appendTo('ul#needs');
-    });
-  });
+  commitButton.toggle(hasModifications);
+}
 
-  // Fetch existing wants
-  $.get('/want', function(wants) {
-    wants.forEach(function(want) {
-      $('<li></li>').text(want.resource + ' - ' + want.amount + ' - ' + want.byWhomForWhom).appendTo('ul#wants');
-    });
+$(document).ready(function() {
+  console.log('Starting here...');
+
+  
+
+  // Set up form submissions
+  submitForm('intake', 'intake');
+  submitForm('need', 'need');
+  submitForm('want', 'want');
+
+  commitTxn('intake', 'intake')
+  commitTxn('need', 'need')
+  commitTxn('want', 'want')
+
+  // Fetch summary
+  fetchSummary();
+
+  // Add refresh button
+  $('#refresh-button').click(function() {
+    fetchSummary();
   });
 });
+
+// Define the fetchSummary function
+function fetchSummary() {
+  // Fetch summary data from the /summary endpoint
+  $.get('/summary', function(summary) {
+    displaySummaryTable('intakes-table', summary.intakes, 'Intakes');
+    displaySummaryTable('needs-table', summary.needs, 'Needs');
+    displaySummaryTable('wants-table', summary.wants, 'Wants');
+  });
+}
+
+// Define the displaySummaryTable function
+function displaySummaryTable(tableId, data, title) {
+  var table = $('#' + tableId);
+  table.empty();
+
+  // Add table title
+  $('<caption></caption>').text(title).appendTo(table);
+
+  // Create and append table header
+  var thead = $('<thead></thead>').appendTo(table);
+  var headerRow = $('<tr><th>txn_id</th><th>Resource</th><th>Amount</th><th>By Whom For Whom</th></tr>').appendTo(thead);
+
+  // Create and append table body
+  var tbody = $('<tbody></tbody>').appendTo(table);
+
+  data.forEach(function(entry) {
+    var row = $('<tr><td>' + entry.txn_id + '</td><td>' + entry.resource + '</td><td>' + entry.amount + '</td><td>' + entry.byWhomForWhom + '</td></tr>').appendTo(tbody);
+  });
+}
--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 import json
 import os
-import sqlite3
 from fastapi import Body, FastAPI, Query, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, Field
+import secrets
+import string
 
-# app = FastAPI(__name__, static_folder='public', template_folder='views')
+def generate_secure_string(length):
+    if not isinstance(length, int) or length <= 0:
+        raise ValueError("Length must be a positive integer.")
+
+    # Generate a random string of alphanumeric characters using secrets module
+    random_chars = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length))
+
+    return random_chars
+
 
 app = FastAPI()
 
@@ -13,12 +24,18 @@ app.mount("/public", StaticFiles(directory="public"), name="public")
 templates = Jinja2Templates(directory="views")
 
 # Set the app secret key from the secret environment variables.
-app.secret = os.environ.get('SECRET')
+app.secret = os.environ.get("SECRET")
+class FormData(BaseModel):
+    txn_id: str = Field(default_factory=lambda: generate_secure_string(8))
+    resource: str
+    amount: int
+    byWhomForWhom: str
 
-# Dream database. Store dreams in memory for now. 
 INTAKES = []
 NEEDS = []
 WANTS = []
+
+# Assuming your data model looks like this
 
 @app.get('/')
 def homepage(request: Request):
@@ -26,18 +43,39 @@ def homepage(request: Request):
     
     return templates.TemplateResponse('index.html', {"request": request})
 
-@app.post('/intake')
-def post_intakes(intake: str = Query(..., title="Intake", description="Intake")):
+@app.get("/summary")
+def get_summary():
+    return {"intakes": INTAKES, "needs": NEEDS, 'wants': WANTS}
 
-    print("Intaking:")
-    print(intake)
 
-    INTAKES.append(intake)
-    return json.dumps(INTAKES)
-  
-@app.get('/intake')
-def get_intakes():
-    return json.dumps(INTAKES)
+@app.post("/commit/intake")
+def post_intake(intakes: list[FormData]):
+    print(f"Processing {len(intakes)} intakes:")
+    print(intakes)
+    for intake in intakes:
 
-if __name__ == '__main__':
-    app.run()
+        INTAKES.append(intake.model_dump())
+
+    return intake
+
+
+@app.post("/commit/need")
+def post_need(needs: list[FormData]):
+    print(f"Processing {len(needs)} Needs:")
+    print(needs)
+    for need in needs:
+
+        NEEDS.append(need.model_dump())
+
+    return need
+
+
+@app.post("/commit/want")
+def post_want(wants: list[FormData]):
+    print(f"Processing {len(wants)} Wants:")
+    print(wants)
+    for want in wants:
+
+        WANTS.append(want.model_dump())
+
+    return want
